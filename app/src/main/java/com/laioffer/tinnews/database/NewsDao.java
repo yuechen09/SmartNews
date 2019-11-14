2060df48d5943504af2987fa313462b27b159848
yuechen <yuechen@bu.edu>
2019-11-13 21:54:50 -0500

    saved news detail

--- app/src/main/java/com/laioffer/tinnews/database/NewsDao.java
+++ app/src/main/java/com/laioffer/tinnews/database/NewsDao.java
@@ -2,12 +2,21 @@ package com.laioffer.tinnews.database;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
 
 import com.laioffer.tinnews.retrofit.response.News;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
 
 @Dao
 public interface NewsDao {
 
     @Insert
     void insertNews(News news);
+
+    @Query("SELECT * FROM news")  // if there is update, return all
+    Flowable<List<News>> getAll();
+
 }
--- app/src/main/java/com/laioffer/tinnews/save/SavedNewsFragment.java
+++ app/src/main/java/com/laioffer/tinnews/save/SavedNewsFragment.java
@@ -1,22 +1,25 @@
 package com.laioffer.tinnews.save;
-
-
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.laioffer.tinnews.R;
-import com.laioffer.tinnews.common.TinBasicFragment;
+import com.laioffer.tinnews.mvp.MvpFragment;
+import com.laioffer.tinnews.retrofit.response.News;
 import com.laioffer.tinnews.save.detail.SavedNewsDetailedFragment;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SavedNewsFragment extends TinBasicFragment {
+public class SavedNewsFragment extends MvpFragment<SavedNewsContract.Presenter> implements SavedNewsContract.View {
 
-
+    private TextView author;
+    private TextView description;
     public static SavedNewsFragment newInstance() {
         Bundle args = new Bundle();
         SavedNewsFragment fragment = new SavedNewsFragment();
@@ -30,12 +33,29 @@ public class SavedNewsFragment extends TinBasicFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_saved_news, container, false);
-        view.findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
+        author = view.findViewById(R.id.author);
+        description = view.findViewById(R.id.description);
+        description.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tinFragmentManager.doFragmentTransaction(SavedNewsDetailedFragment.newInstance());
             }
         });
         return view;
+    }
+
+    @Override
+    public SavedNewsContract.Presenter getPresenter() {
+        return new SavedNewsPresenter();
+    }
+
+
+    @Override
+    public void loadSavedNews(List<News> newsList) {
+        if (newsList.size() > 0) {
+            News news = newsList.get(newsList.size() - 1);
+            author.setText(news.getAuthor());
+            description.setText(news.getDescription());
+        }
     }
 }
